1e25239a9884b57686871c90bec27ff2ae8835ef
André Felipe C. Leite <andrefelipecl@outlook.com>
2020-12-09 21:48:16 -0300

    Tela de Listagem de Ordem de Serviços com filtro

--- src/app/service-order/service-order-list/service-order-list.component.ts
+++ src/app/service-order/service-order-list/service-order-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-service-order-list',
-  templateUrl: './service-order-list.component.html',
-  styleUrls: ['./service-order-list.component.css']
+	selector: 'app-service-order-list',
+	templateUrl: './service-order-list.component.html',
+	styleUrls: ['./service-order-list.component.css']
 })
 export class ServiceOrderListComponent implements OnInit {
 
-  constructor() { }
+	clientName: string;
+	months: number[];
+	month: number;
+	year: number;
 
-  ngOnInit(): void {
-  }
+	constructor() {
+		this.months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+	}
 
+	ngOnInit(): void {}
+
+	searchFilter(){
+		console.log(this.clientName);
+		console.log(this.month);
+		console.log(this.year);
+	}
 }
